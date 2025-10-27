55b66dc638f5bd9d8267d7bcdcbed9fe178d1a03
Sriram-Hasini <sriramhasini613@gmail.com>
2025-10-27 12:21:13 +0530

    Delete app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, request, jsonify
-app = Flask(__name__)
-
-@app.route('/analyze', methods=['POST'])
-def analyze():
-    return jsonify({"attentive_score": 87, "speech_turns": 5})
-
-if __name__ == "__main__":
-    app.run(debug=True)
